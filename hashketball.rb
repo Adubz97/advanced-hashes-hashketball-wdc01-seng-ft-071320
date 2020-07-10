d2992cd7730cb9c302b08d035dae886db566ddd3
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-07-10 17:17:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,7 +162,7 @@ def team_names()
   end
 end
 
-def player_numbers(team_name)
+def player_numbers(team)
   game_hash.values.each do |team|
     team[:players].each do |player|
       if player.has_value? player_numbers
